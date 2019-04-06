f3e52612c12863ee1ca88c41b4a03842ad6573eb
zhuxiaopan <17723600030@163.com>
2019-04-06 15:43:59 +0800

    提交测试代码

---
+++ aprilTest/src/main/java/com/company/git/GitTest.java
@@ -0,0 +1,17 @@
+package com.company.git;
+
+/**
+ * @Author: zhuxiaopan
+ * @createDate: 2019/4/6 15:33
+ * @Package: com.company.git
+ * @Project: aprilTest
+ * @Version: 1.0
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+    }
+}
